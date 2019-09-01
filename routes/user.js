e67acd8b63b77cf84bad03a339916049013f760e
Darren SooHoo <darsoohoo@gmail.com>
2019-09-01 16:53:39 -0700

    added admin authorization

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { signup, signin, signout, requireSignin } = require("../controllers/user");
-const { userSignupValidator } = require("../validator");
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth")
+const { userById } = require("../controllers/user");
 
-router.post("/signup", userSignupValidator, signup);
-router.post("/signin", signin);
-router.get("/signout", signout);
+router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
+    res.json({
+        user: req.profile
+    });
+});
+
+
+router.param("userId", userById);
 
 
 module.exports = router;
